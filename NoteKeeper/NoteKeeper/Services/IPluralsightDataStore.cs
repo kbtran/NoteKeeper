68b09b52887340f0a4e5a4fab721a2b38bde9049
Bruce Tran <bruce.tran@gmail.com>
2020-04-22 14:25:26 -0400

    Add mock courses

---
+++ NoteKeeper/NoteKeeper/Services/IPluralsightDataStore.cs
@@ -0,0 +1,16 @@
+﻿using NoteKeeper.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NoteKeeper.Services
+{
+    public interface IPluralsightDataStore
+    {
+        Task<String> AddNoteAsync(Note courseNote);
+        Task<bool> UpdateNoteAsync(Note courseNote);
+        Task<Note> GetNoteAsync(String id);
+        Task<IList<Note>> GetNotesAsync();
+        Task<IList<String>> GetCoursesAsync();
+    }
+}
